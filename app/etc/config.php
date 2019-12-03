c591ed052f9912c28c796b2eee5685185b892185
Marco Oliveira <marcoaacoliveira@gmail.com>
2019-12-03 18:38:51 -0300

    Adding a sample of acl.xml

--- app/etc/config.php
+++ app/etc/config.php
@@ -213,6 +213,7 @@ return [
         'Klarna_Kp' => 1,
         'Magento_PaypalReCaptcha' => 1,
         'MSP_TwoFactorAuth' => 1,
+        'Marco_Configuration' => 1,
         'Marco_MinimalModule' => 1,
         'Temando_Shipping' => 1,
         'Vertex_Tax' => 1
